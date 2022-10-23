355a2b84df9e0e65390fc7f94ff2c7d53c509dc5
Ömer Algin <oemer-algin@hotmail.de>
2022-10-23 17:53:06 +0200

    slider

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
+import ProductList from "../components/Productlist";
 import Slider from "../components/Slider";
 
 export default function Home() {
   return (
     <div>
       <Slider />
-      <h1>Essen</h1>
+      <ProductList />
     </div>
   );
 }
