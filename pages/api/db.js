491d0cf1312accfd563b72fe2c18617cd8c8c4fb
Ömer Algin <oemer-algin@hotmail.de>
2022-10-29 19:15:11 +0200

    install mongoose, brew, mongodb-community

---
+++ pages/api/db.js
@@ -0,0 +1,10 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import { mongo } from "mongoose";
+import mongodb from "../../utils/mongodb";
+
+export default function handler(req, res) {
+  mongodb.dbConnect();
+  mongodb.dbDisconnect();
+  res.status(200).json({ name: "John Doe" });
+}
