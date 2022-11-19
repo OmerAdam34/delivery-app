5623acdaa172a66de12074ae1cc4c5fe8fc27660
Ömer Algin <oemer-algin@hotmail.de>
2022-11-19 20:53:37 +0100

    uuid, add order to shoppingcart

--- pages/shoppingCart.jsx
+++ pages/shoppingCart.jsx
@@ -3,79 +3,88 @@ import Image from "next/image";
 import Link from "next/link";
 import CardHeader from "react-bootstrap/esm/CardHeader";
 import { useDispatch, useSelector } from "react-redux";
+import { deleteProduct } from "../redux/warenkorbSlice";
 
 export default function ShoppingCart() {
   const dispatch = useDispatch();
   const shoppingCart = useSelector((state) => state.shoppingCart);
 
-  const entfernen = (product) => {};
+  const entfernen = (product) => {
+    dispatch(deleteProduct(product));
+  };
 
   return (
     <div>
-      <h1>Warenkorb</h1>
-      <div className="row mt-4">
-        <div className="col-9">
-          <Table hover responsive>
-            <thead>
-              <tr>
-                <th>Picture</th>
-                <th>Name</th>
-                <th>Extras</th>
-                <th>Quantity</th>
-                <th>Price</th>
-                <th>
-                  <CloseButton disabled />
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {shoppingCart.products.map((product) => (
-                <tr key={product._id}>
-                  <td>
-                    <Image
-                      src={product.picture}
-                      alt={product.name}
-                      width={50}
-                      height={50}
-                    />
-                  </td>
-                  <td>
-                    <Link href={`/products/${product.url}`}>
-                      <a className="text-danger">{product.name}</a>
-                    </Link>
-                  </td>
-                  <td>
-                    {product.extras.map((extra) => (
-                      <span key={extra._id}>{extra.text}</span>
-                    ))}
-                  </td>
-                  <td>{product.quantity}</td>
-                  <td>{(product.price * product.quantity).toFixed(2)}</td>
-                  <td>
-                    <Button
-                      className="btn-sm"
-                      onClick={() => entfernen(product)}
-                    >
-                      x
-                    </Button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </Table>
-        </div>
-        <div className="col-3 p-2">
-          <div>
-            <Card>
-              <CardHeader as="h5">Total</CardHeader>
-              <Card.Body className="text-center">
-                <Card.Title>{shoppingCart.amount.toFixed(2)}</Card.Title>
-                <Button variant="primary">To Checkout</Button>
-              </Card.Body>
-            </Card>
+      {shoppingCart.quantity === 0 ? (
+        <h2>Der Warenkorb ist leer</h2>
+      ) : (
+        <div>
+          <h1>Warenkorb</h1>
+          <div className="row mt-4">
+            <div className="col-9">
+              <Table hover responsive>
+                <thead>
+                  <tr>
+                    <th>Picture</th>
+                    <th>Name</th>
+                    <th>Extras</th>
+                    <th>Quantity</th>
+                    <th>Price</th>
+                    <th>
+                      <CloseButton disabled />
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {shoppingCart.products.map((product) => (
+                    <tr key={product._id}>
+                      <td>
+                        <Image
+                          src={product.picture}
+                          alt={product.name}
+                          width={50}
+                          height={50}
+                        />
+                      </td>
+                      <td>
+                        <Link href={`/products/${product.url}`}>
+                          <a className="text-danger">{product.name}</a>
+                        </Link>
+                      </td>
+                      <td>
+                        {product.extras.map((extra) => (
+                          <span key={extra._id}>{extra.text}</span>
+                        ))}
+                      </td>
+                      <td>{product.quantity}</td>
+                      <td>{(product.price * product.quantity).toFixed(2)}</td>
+                      <td>
+                        <Button
+                          className="btn-sm"
+                          onClick={() => entfernen(product)}
+                        >
+                          x
+                        </Button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+            </div>
+            <div className="col-3 p-2">
+              <div>
+                <Card>
+                  <CardHeader as="h5">Total</CardHeader>
+                  <Card.Body className="text-center">
+                    <Card.Title>{shoppingCart.amount.toFixed(2)}</Card.Title>
+                    <Button variant="primary">To Checkout</Button>
+                  </Card.Body>
+                </Card>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
